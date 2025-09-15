31136970b8db22cb34629851725a60cb1688a747
dextey <dexteydev@gmail.com>
2025-09-15 15:38:22 +0530

    updated ques rank type based on index

--- src/components/BatchSelector.jsx
+++ src/components/BatchSelector.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-const batches = Array.from({ length: 7 }, (_, i) => i + 1);
+const batches = Array.from({ length: 10 }, (_, i) => i + 1);
 
 export default function BatchSelector({ onSelectBatch }) {
   const [progress, setProgress] = useState({});
@@ -18,17 +18,36 @@ export default function BatchSelector({ onSelectBatch }) {
         {batches.map((num) => {
           const batchKey = `batch-${num}`;
           const batchData = progress[batchKey];
+          let hard = null;
+          if (num === 9) hard = "HARD";
+          if (num === 10) hard = "MATH";
 
           return (
             <div
               key={num}
               onClick={() => onSelectBatch(num)}
-              className={`flex justify-between items-center p-4 border rounded-lg shadow cursor-pointer hover:bg-blue-50 transition
-                ${
-                  batchData?.completed ? "border-green-500" : "border-gray-300"
-                }`}
+              className={`flex justify-between items-center p-4 border rounded-lg shadow cursor-pointer hover:bg-blue-50 transition relative
+                ${batchData?.completed ? "border-green-500" : "border-gray-300"}
+                `}
             >
+              {hard && (
+                <div
+                  className={`px-3 text-white w-15 absolute text-center text-[.71rem] -rotate-90 left-[-23px] rounded-t-lg ${
+                    hard === "MATH" ? "bg-blue-400" : "bg-red-400"
+                  }
+                  
+                   ${
+                     batchData?.completed
+                       ? "w-18 left-[-28px]"
+                       : "border-gray-300"
+                   }
+                  `}
+                >
+                  {hard}
+                </div>
+              )}
               {/* Title */}
+
               <h2 className="text-lg font-semibold">Questions Set {num}</h2>
 
               {/* Status */}
