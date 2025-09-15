ecc5cbbfbbca3c2d96311e1b0a43b1fee85edb67
dextey <dexteydev@gmail.com>
2025-09-15 15:37:17 +0530

    added new questionts and updated mappings

--- src/questions/index.js
+++ src/questions/index.js
@@ -6,6 +6,12 @@ import batch5 from "./questions_batch_5.json";
 import batch6 from "./questions_batch_6.json";
 import batch7 from "./nursing_batch6_calc_25.json";
 
+// new set
+
+import batch8 from "./new1.json";
+import batch9 from "./new2.json";
+import batch10 from "./new3.json";
+
 export const questionBatches = {
   1: batch1,
   2: batch2,
@@ -14,4 +20,7 @@ export const questionBatches = {
   5: batch5,
   6: batch6,
   7: batch7,
+  8: batch8,
+  9: batch9,
+  10: batch10,
 };
